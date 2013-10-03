44d37c7082c241922aa987e9709c603b6cf9bc4e
yoshikazu.nakase <yoshikazu.nakase@access-company.com>
2013-10-03 21:21:18 +0900

    [mod] add gitignore file

--- spec/kinvey-rails_spec.rb
+++ spec/kinvey-rails_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "KinveyRails" do
   before(:all) do
-    @kinvey = Kinvey.init('kid_VTkkODHk1M', '79b2f9757285425f88aa155f87b44dd5')
+    @kinvey = Kinvey.init(APP_KEY, APP_SECRET)
   end
 
   it "init Kinvey" do
